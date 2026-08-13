5eca34aa3b09f607fb7cd44a44a516259ac97b21
eduarda <eduarda.costa6016@gmail.com>
2026-08-13 20:53:03 -0300

    Reorganizing project

--- portfolio/scripts/exemplo.js
+++
@@ -1,9 +0,0 @@
-// Entrada
-const valorJantar = prompt("Valor do jantar = R$ ")
-
-// Processamento
-const valorGarcom = Number(valorJantar) * 0.10
-const valorTotal = Number(valorJantar) + valorGarcom
-
-// Saída
-alert("Valor total: R$ " + valorTotal);
